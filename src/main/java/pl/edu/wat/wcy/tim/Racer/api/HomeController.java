be21d0b1ecf6bdcf15bbfb0b760cca4edfe09c0e
kamil9115 <kamil9115@gmail.com>
2018-08-10 17:28:09 +0200

    IU-2017.2 <Kamil@Yoga-710

--- src/main/java/pl/edu/wat/wcy/tim/Racer/api/HomeController.java
+++ src/main/java/pl/edu/wat/wcy/tim/Racer/api/HomeController.java
@@ -1,12 +1,16 @@
-package pl.edu.wat.wcy.tim.Racer.api;
+package pl.edu.wat.wcy.tim.racer.api;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Created by Kamil on 25.07.2017.
+ */
 @Controller
 public class HomeController {
+
     @RequestMapping(value="/")
-    public String home(){
-        return "forward:index.html";
+    public String home() {
+        return "forward:home.html";
     }
 }
--- src/main/java/pl/edu/wat/wcy/tim/Racer/domain/Uzytkownik.java
+++ src/main/java/pl/edu/wat/wcy/tim/Racer/domain/Uzytkownik.java
@@ -1,20 +1,22 @@
-package pl.edu.wat.wcy.tim.Racer.domain;
+package pl.edu.wat.wcy.tim.racer.domain;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+/**
+ * Created by Kamil on 25.07.2017.
+ */
 @Entity
+@Table(name="Uzytkownik",catalog="Racer",schema="dbo")
 public class Uzytkownik {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    @Column(length=30,nullable=false)
+    @Column(length=30,nullable = false)
     private String nazwa;
-    @Column(length=30,nullable=false)
+    @Column(length=30,nullable = false)
     private String haslo;
-    @Column(nullable=false)
+    @Column(nullable = false)
     private boolean administrator;
 
     public Uzytkownik(){}
@@ -28,31 +30,30 @@ public class Uzytkownik {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
 
+
     public String getNazwa() {
         return nazwa;
     }
-
     public void setNazwa(String nazwa) {
         this.nazwa = nazwa;
     }
 
+
     public String getHaslo() {
         return haslo;
     }
-
     public void setHaslo(String haslo) {
         this.haslo = haslo;
     }
 
+
     public boolean getAdministrator() {
         return administrator;
     }
-
     public void setAdministrator(boolean administrator) {
         this.administrator = administrator;
     }
